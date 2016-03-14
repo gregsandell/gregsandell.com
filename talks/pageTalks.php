37a8bd5ea7c38b19f6dee13f0eca2a09c8ad3052
Greg Sandell <greg.sandell@gmail.com>
2016-03-13 17:37:41 -0700

    File reorg:  make talks files to own /talks folder

--- talks/pageTalks.php
+++ talks/pageTalks.php
@@ -2,7 +2,7 @@
 	$_cpath = $_SERVER['DOCUMENT_ROOT'];
 	$g_section = "talks";
 	$linksTitle = "Other Site Links";
-	$inlinePhp = "/innerPageTalks.php";
+	$inlinePhp = $_cpath . "/talks/innerPageTalks.php";
 	
 	$mainCopy = "";
 include($_cpath . "/objSidebar.php");
