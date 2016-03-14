4ce4a20476b6ffb69f4b633fc9505e79e0e2df77
Greg Sandell <greg.sandell@gmail.com>
2016-03-14 16:27:02 -0700

    File reorg:  eliminate use of cdn's for jQuery and jQuery mobile resources, use files on server instead; rename image folder because of changed jQuery version; place unused file in notInUse folder (to remove later)

--- j/inclHead.php
+++ j/inclHead.php
@@ -1,17 +1,3 @@
-<?php
-/*
-VERSION 4.1  with jQuery 1.6
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
-<script src="http://code.jquery.com/jquery-1.6.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
-
-VERSION 3 with jQuery 1.5
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.css" />
-<script src="http://code.jquery.com/jquery-1.5.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.js"></script>
-*/
-?>
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
-<script src="http://code.jquery.com/jquery-1.6.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
-
+<link rel="stylesheet" href="/j/jquery/jquery.mobile-1.0a4.1.min.css" />
+<script src="/j/jquery/jquery-1.6.1.min.js"></script>
+<script src="/j/jquery/jquery.mobile-1.0a4.1.min.js"></script>
--- j/notInUse/resume.php
+++ j/notInUse/resume.php
@@ -89,21 +89,7 @@ foreach ($skills->categoryObjArray as $skillCategory) {
 <html>
 <head>
   <title>GregSandell.com Mobile</title> 
-<!--
-VERSION 4.1  with jQuery 1.6
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
-<script src="http://code.jquery.com/jquery-1.6.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
-
-VERSION 3 with jQuery 1.5
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.css" />
-<script src="http://code.jquery.com/jquery-1.5.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.0a3/jquery.mobile-1.0a3.min.js"></script>
--->
-
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
-<script src="http://code.jquery.com/jquery-1.6.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
+  <?php include("inclHead.php"); ?>
 </head>
 <body>
 
