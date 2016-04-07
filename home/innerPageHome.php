fa1715a760b2fe6d2d500ab8fcdc94edd887548b
Greg Sandell <greg.sandell@gmail.com>
2016-04-06 23:18:09 -0700

    Update home page copy to reflect resume summary

--- home/innerPageHome.php
+++ home/innerPageHome.php
@@ -12,7 +12,10 @@
 <h1>Frontend/Mobile Web Developer</h1>
 <p style='text-align: justify; float: left; padding-right: 0px'>
 	&nbsp;<img alt="Photo of Greg Sandell" style='float: right; padding-left: 5px' src='/image/gregHeadshot-50.jpg' />
-	I am a 14-year Front End Developer of web applications with particular depth in Mobile Web, Javascript, AngularJS, Single Page Web Applications, jQuery, Ajax/JSON, web services, HTML5, CSS3, Responsive Design, Unix, Agile and Open Source. I have worked for Reuters, Chicago Tribune, UBS, Amex, Lockheed-Martin, and Sears in vertical markets including media, travel, finance, defense, healthcare, retail, eCommerce and education.  I hold a PhD degree and am highly effective at communications, writing and presentations.  
+I have 18 years experience in Front End Development of web-based applications, playing a delivery role in medium to large enterprise projects in Java/Linux/Open Source environments. I focus on the Front End programming logic for transactional web pages, and can deploy designs in HTML5 and CSS3 as well. I have 13 years experience with Agile methodology, tooling, TDD and test coverage.
+</p>
+<p>
+Technology stack (ordered roughly by recency and strength): AngularJS 1.4, Javascript, Git, Mocha/Sinon/Chai, Advanced Rest Client, WebStorm 11.0.3, Rally, Jenkins, LESS, nodeJS, API Blueprint, Nexus, Bootstrap, ExtJS 4.0.7, Jasmine 2.3, Fisheye/Crucible, Eclipse 4.4/Luna, jMeter 2.13, Oracle, jQuery, jQuery Mobile, Maven 4, HTML5, CSS3, Tomcat 6, GlassFish 3.1.2, JEE 6, JSP, Artifactory, mysql, SQL Server.</p>
 </p>
 <h2>Switch to the <a href="/j">Mobile version</a> of the site, written in jQuery Mobile!</h2>
 
