7a079c15848cc5c91c46bdc6a1c630fc195f8390
Greg Sandell <greg.sandell@gmail.com>
2017-06-06 14:33:13 -0700

    Add 'see more' instruction to portfolio home page

--- portfolio/templatePortfolio.php
+++ portfolio/templatePortfolio.php
@@ -104,6 +104,7 @@
 									</td>
 								</tr>
 							</table>
+							<p><i>(Click on image below for more detail)</i></p>
 							<a href="/portfolio/pages/pageClient_<?php print($key) ?>.php">
 								<img src="<?php print($client->image) ?>" width="<?php print($client->width) ?>" height="<?php print($client->height) ?>" border="<?php  print($imageBorder) ?>" valign="top"/>
 							</a>
