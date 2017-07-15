21a241cee1d259d9430de6a3e553dd5b67f60e90
Greg Sandell <greg.sandell@gmail.com>
2017-07-15 10:54:48 -0700

    Eliminate “portfolio popup” approach to displaying publications

--- portfolio/pages/pagePublications.php
+++ portfolio/pages/pagePublications.php
@@ -13,27 +13,27 @@ various disciplines relating to music and human hearing: auditory grouping,
 Digital Signal Processing for music synthesis, acoustics of musical instruments, and orchestration.
 </p>
 <p>Want to see my
-<a href='/portfolio/portfolioPopup.php?url=http://classic-web.archive.org/web/19990909140857re_/sparky.parmly.luc.edu/sandell' TARGET=_BLANK>1995 Home Page</a>
-from those days?  (Courtesy of the <a href='/portfolio/portfolioPopup.php?url=http://wayback.archive.org/web/' TARGET=_BLANK>Internet Archive Wayback Machine</a>)
+<a href='http://classic-web.archive.org/web/19990909140857re_/sparky.parmly.luc.edu/sandell'>1995 Home Page</a>
+from those days?  (Courtesy of the <a href='http://wayback.archive.org/web/'>Internet Archive Wayback Machine</a>)
 </p>
 <dl>
 <dt>Links to Peer Review Articles</dt>
 <dd><b>Music Perception</b>: 
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1995-01-01_roles_MP.pdf' TARGET=_BLANK>1995</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1987-01-01_perception_MP.pdf' TARGET=_BLANK>1987</a><br/>
+<a href='/portfolio/publications/1995-01-01_roles_MP.pdf'>1995</a>,
+<a href='/portfolio/publications/1987-01-01_perception_MP.pdf'>1987</a><br/>
 <b>Journal of the Audio Engineering Society</b>: 
-<a  href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1995-12-01_perceptual_JAES.pdf' TARGET=_BLANK>1995</a><br/>
+<a href='/portfolio/publications/1995-12-01_perceptual_JAES.pdf'>1995</a><br/>
 <b>Journal of the Acoustical Society America</b>:  
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1995-05-01_absence_JASA.pdf' TARGET=_BLANK>1995</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1994-05-01_effect_JASA.pdf' TARGET=_BLANK>1994</a>
+<a href='/portfolio/publications/1995-05-01_absence_JASA.pdf'>1995</a>,
+<a href='/portfolio/publications/1994-05-01_effect_JASA.pdf'K>1994</a>
 </dd>
 <dt>Dissertation</dt>
 <dd>
 PhD Dissertation, Northwestern University, 1991.
 Title:
-'Concurrent Timbres in Orchestration: a Perceptual Study of Factors Determining 'blend'<br/>
+<i>Concurrent Timbres in Orchestration: a Perceptual Study of Factors Determining 'blend'</i><br/>
 
-<a href='/portfolio/portfolioPopup.php?url=http://adsabs.harvard.edu/abs/1991PhDT.......313S' TARGET=_BLANK>Online abstract</a>.
+<a href='http://adsabs.harvard.edu/abs/1991PhDT.......313S'>Online abstract</a>.
 <br/>As a series of PDFs:
 <span style='font-size: 9px'>
 <a href='/portfolio/publications/dissertation/in_parts/01_cover_TOC.pdf'>1 (4.9 mb)</a>
@@ -48,32 +48,32 @@ Title:
 <dt>Links to Articles in Conference Proceedings</dt>
 
 <dd><b>Acoustical Society of America</b>:  
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1998-06-20_macrotimbre_ASA.pdf' TARGET=_BLANK>1998</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1996-12-04_recognition_ASA.pdf' TARGET=_BLANK>1996a</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1996-12-05_identifying_ASA.pdf' TARGET=_BLANK>1996b</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1994-06-09_effect_ASA.pdf' TARGET=_BLANK>1994a</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1994-06-09_analysis_ASA.pdf' TARGET=_BLANK>1994b</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1989-11-29_effect_ASA.pdf' TARGET=_BLANK>1989</a>
+<a href='/portfolio/publications/1998-06-20_macrotimbre_ASA.pdf'>1998</a>,
+<a href='/portfolio/publications/1996-12-04_recognition_ASA.pdf'>1996a</a>,
+<a href='/portfolio/publications/1996-12-05_identifying_ASA.pdf'>1996b</a>,
+<a href='/portfolio/publications/1994-06-09_effect_ASA.pdf'>1994a</a>,
+<a href='/portfolio/publications/1994-06-09_analysis_ASA.pdf'>1994b</a>,
+<a href='/portfolio/publications/1989-11-29_effect_ASA.pdf'>1989</a>
 <br/>
 <b>International Computer Music Conference</b>: 
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1992-10-14_prototyping_ICMC.pdf' TARGET=_BLANK>1992</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1991-10-01_library_ICMC.pdf' TARGET=_BLANK>1991</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1989-01-01_perception_ICMC.pdf' TARGET=_BLANK>1989</a><br/>
+<a href='/portfolio/publications/1992-10-14_prototyping_ICMC.pdf'>1992</a>,
+<a href='/portfolio/publications/1991-10-01_library_ICMC.pdf'>1991</a>,
+<a href='/portfolio/publications/1989-01-01_perception_ICMC.pdf'>1989</a><br/>
 <b>Society for Music Perception &amp; Cognition</b>:  
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1997-06-07_perceptual_ESCOM.pdf' TARGET=_BLANK>1997a</a>,
-<a  href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1997-07-31_perceptual_SMPC.pdf' TARGET=_BLANK>1997b</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1996-08-11_sharc_ICMPC.pdf' TARGET=_BLANK>1996</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1995-06-22_grouping_SMPC.pdf' TARGET=_BLANK>1995a</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1995-06-22_perceptual_SMPC.pdf' TARGET=_BLANK>1995b</a>,
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1992-02-22_factors_ICMPC.pdf' TARGET=_BLANK>1992</a>
+<a href='/portfolio/publications/1997-06-07_perceptual_ESCOM.pdf'>1997a</a>,
+<a  href='/portfolio/publications/1997-07-31_perceptual_SMPC.pdf'>1997b</a>,
+<a href='/portfolio/publications/1996-08-11_sharc_ICMPC.pdf'>1996</a>,
+<a href='/portfolio/publications/1995-06-22_grouping_SMPC.pdf'>1995a</a>,
+<a href='/portfolio/publications/1995-06-22_perceptual_SMPC.pdf'>1995b</a>,
+<a href='/portfolio/publications/1992-02-22_factors_ICMPC.pdf'>1992</a>
 <br/>
 <b>Society for Music Theory</b>:  
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1989-10-26_timbre_SMT.pdf' TARGET=_BLANK>1989</a></dd>
+<a href='/portfolio/publications/1989-10-26_timbre_SMT.pdf'>1989</a></dd>
 <dt>Links to Book Reviews</dt>
 <dd><b>Music Perception</b>:  
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1996-06-01_auditory_MP.pdf' TARGET=_BLANK>1996</a><br/>
+<a href='/portfolio/publications/1996-06-01_auditory_MP.pdf'>1996</a><br/>
 <b>Music Theory Spectrum</b>:  
-<a href='/portfolio/portfolioPopup.php?url=/portfolio/publications/1990-09-01_soundColor_MTS.pdf' TARGET=_BLANK>1990</a></dd>
+<a href='/portfolio/publications/1990-09-01_soundColor_MTS.pdf'>1990</a></dd>
 </dl>
 
 
