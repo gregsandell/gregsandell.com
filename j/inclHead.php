00c180d731e1e4f259873d4d867f961cd9fabd1b
Greg Sandell <greg.sandell@gmail.com>
2017-07-29 20:51:27 -0700

    Add support for mobile bookmark icons

--- j/inclHead.php
+++ j/inclHead.php
@@ -1,3 +1,10 @@
+<LINK REL="SHORTCUT ICON" HREF="/image/icons/favicon.ico" type="image/x-icon" />
+<link href="/image/icons/apple-touch-icon.png" rel="apple-touch-icon" />
+<link href="/image/icons/apple-touch-icon-152x152.png" rel="apple-touch-icon" sizes="152x152" />
+<link href="/image/icons/apple-touch-icon-167x167.png" rel="apple-touch-icon" sizes="167x167" />
+<link href="/image/icons/apple-touch-icon-180x180.png" rel="apple-touch-icon" sizes="180x180" />
+<link href="/image/icons/icon-hires.png" rel="icon" sizes="192x192" />
+<link href="/image/icons/icon-normal.png" rel="icon" sizes="128x128" />
 <link rel="stylesheet" href="/lib/jquerymobile/1.0a4.1/jquery.mobile-1.0a4.1.min.css" />
 <script src="/lib/jquery/1.6.1/jquery-1.6.1.min.js"></script>
 <script src="/lib/jquerymobile/1.0a4.1/jquery.mobile-1.0a4.1.min.js"></script>
