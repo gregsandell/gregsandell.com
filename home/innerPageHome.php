7583e28421590e94f5e667aa33c6698c50c33c48
Greg Sandell <GSandell@contractor.crocs.com>
2018-12-14 11:09:10 -0800

    Fix spelling of webpack

--- home/innerPageHome.php
+++ home/innerPageHome.php
@@ -9,7 +9,7 @@
 	&nbsp;<img alt="Photo of Greg Sandell" style='float: right; padding-left: 5px' src='/image/gregHeadshot-50.jpg' />I have over ten years experience in Full Stack Development of web-based applications, playing a delivery role in medium to large enterprise projects in NodeJs, Java and Open Source environments.  I focus on the Front End programming logic for transactional web pages, Automated UI tests and deploying designs in HTML5 and CSS3.
 </p>
 <p>
-<strong>Technology stack</strong> (ordered roughly by recency and strength):  Javascript/ES6/NodeJs, React 15, Redux, Web pack, Git, GitHub, BitBucket, npm, yarn, AWS, Linux, Yum, AngularJS 1.4, Nightwatch, InternJS, Selenium, BrowserStack, HTML5, CSS3, Postman, Advanced Rest Client, nginx, sqlite, Bootstrap, SASS, jQuery, Firebase, Jenkins, Nexus, Artifactory, Bugzilla, Jasmine/Mocha/Sinon/Chai, LaunchDarkly, WebStorm, Rally, Sencha ExtJS 4.0.7, Atlassian code tools, Eclipse, Oracle, Stored Procedures, Maven 4, Tomcat 6, GlassFish 3.1.2, JEE 6, JSP, Mysql, SQL Server.
+<strong>Technology stack</strong> (ordered roughly by recency and strength):  Javascript/ES6/NodeJs, React 15, Redux, Webpack, Git, GitHub, BitBucket, npm, yarn, AWS, Linux, Yum, AngularJS 1.4, Nightwatch, InternJS, Selenium, BrowserStack, HTML5, CSS3, Postman, Advanced Rest Client, nginx, sqlite, Bootstrap, SASS, jQuery, Firebase, Jenkins, Nexus, Artifactory, Bugzilla, Jasmine/Mocha/Sinon/Chai, LaunchDarkly, WebStorm, Rally, Sencha ExtJS 4.0.7, Atlassian code tools, Eclipse, Oracle, Stored Procedures, Maven 4, Tomcat 6, GlassFish 3.1.2, JEE 6, JSP, Mysql, SQL Server.
 </p>
 <h2>Switch to the <a href="/j">Mobile version</a> of the site, written in jQuery Mobile!</h2>
 
