68850e5a2fe32a995494205bbf72d5c32e2f211f
Greg Sandell <greg.sandell@gmail.com>
2019-11-29 14:16:48 -0800

    Change Phd and Masters thesis pages:  give chapter titles, put title of thesis on thesis page

--- portfolio/innerPageDissertation.php
+++ portfolio/innerPageDissertation.php
@@ -5,7 +5,7 @@
 
 ?>
 <header>
-	<h1>Music</h1>
+	<h1>PhD Thesis: <i>Concurrent Timbres in Orchestration: a Perceptual Study of Factors Determining 'blend'</i></h1>
 	<p>
 	  Here are links to the Dissertation I wrote for my PhD in Music from Northwestern University.
 	<p/>
@@ -16,11 +16,11 @@
 		<dd>
 			<ul>
 				<li><a href='/portfolio/publications/dissertation/01_cover_TOC.pdf'>Cover, Table of Contents, etc. (4.9 mb)</a></li>
-				<li><a href='/portfolio/publications/dissertation/02_chapter_1.pdf'>Chapter 1 (23.9 mb)</a></li>
-				<li><a href='/portfolio/publications/dissertation/03_chapter_2.pdf'>Chapter 2 (8.6 mb)</a></li>
-				<li><a href='/portfolio/publications/dissertation/04_chapter_3.pdf'>Chapter 3 (23.7 mb)</a></li>
-				<li><a href='/portfolio/publications/dissertation/05_chapter_4.pdf'>Chapter 4 (36.8 mb)</a></li>
-				<li><a href='/portfolio/publications/dissertation/06_chapter_5.pdf'>Chapter 5 (5 mb)</a></li>
+				<li><a href='/portfolio/publications/dissertation/02_chapter_1.pdf'>Chapter 1: Introduction (23.9 mb)</a></li>
+				<li><a href='/portfolio/publications/dissertation/03_chapter_2.pdf'>Chapter 2: Perceptual Research and "Blend" (8.6 mb)</a></li>
+				<li><a href='/portfolio/publications/dissertation/04_chapter_3.pdf'>Chapter 3: Method for Investigating "Blend" (23.7 mb)</a></li>
+				<li><a href='/portfolio/publications/dissertation/05_chapter_4.pdf'>Chapter 4: Experiments in Blend Perception (36.8 mb)</a></li>
+				<li><a href='/portfolio/publications/dissertation/06_chapter_5.pdf'>Chapter 5: Concluding Remarks (5 mb)</a></li>
 				<li><a href='/portfolio/publications/dissertation/07_references.pdf'>References (4.3 mb)</a></li>
 			</ul>
 		</dd>
--- portfolio/innerPageThesis.php
+++ portfolio/innerPageThesis.php
@@ -5,7 +5,7 @@
 
 ?>
 <header>
-	<h1>Music</h1>
+	<h1>Masters Degree Thesis: <i>Edgard Varese's <u>Ecuatorial</u>; A Pitch-Class Set Analysis</i></h1>
 	<p>
 	  Here are links to the Thesis I wrote for my Masters Degree in Music from Eastman School of Music.
 	<p/>
@@ -16,13 +16,14 @@
 		<dd>
 			<ul>
 				<li><a href='/portfolio/publications/mastersThesis/01_cover_TOC.pdf'>Cover, Table of Contents, etc. (141 kb)</a></li>
-				<li><a href='/portfolio/publications/mastersThesis/02_chapter_1.pdf'>Chapter 1 (1 mb)</a></li>
-				<li><a href='/portfolio/publications/mastersThesis/03_chapter_2.pdf'>Chapter 2 (515 kb)</a></li>
-				<li><a href='/portfolio/publications/mastersThesis/04_chapter_3.pdf'>Chapter 3 (464 kb)</a></li>
-				<li><a href='/portfolio/publications/mastersThesis/05_chapter_4.pdf'>Chapter 4 (2.4 mb)</a></li>
-				<li><a href='/portfolio/publications/mastersThesis/07_Appendix_01.pdf'>Appendix 1 (74 kb)</a></li>
-				<li><a href='/portfolio/publications/mastersThesis/08_Appendix_02.pdf'>Appendix 2 (255 kb)</a></li>
-				<li><a href='/portfolio/publications/mastersThesis/09_Appendix_03.pdf'>Appendix 3 (1.1 mb)</a></li>
+				<li><a href='/portfolio/publications/mastersThesis/02_chapter_1.pdf'>Chapter 1: Recent Theoretical Writings on the Music of Varèse (1 mb)</a></li>
+				<li><a href='/portfolio/publications/mastersThesis/03_chapter_2.pdf'>Chapter 2: The Analytic Method (515 kb)</a></li>
+				<li><a href='/portfolio/publications/mastersThesis/04_chapter_3.pdf'>Chapter 3: Preliminary Analysis (464 kb)</a></li>
+				<li><a href='/portfolio/publications/mastersThesis/05_chapter_4.pdf'>Chapter 4: An Analysis of <u>Ecuatorial</u> (2.4 mb)</a></li>
+				<li><a href='/portfolio/publications/mastersThesis/06_Bibliography.pdf'>Bibliography (2.4 mb)</a></li>
+				<li><a href='/portfolio/publications/mastersThesis/07_Appendix_01.pdf'>Appendix 1: The Text of <u>Ecuatorial</u> (74 kb)</a></li>
+				<li><a href='/portfolio/publications/mastersThesis/08_Appendix_02.pdf'>Appendix 2: Score Accuracy in <u>Ecuatorial</u> (255 kb)</a></li>
+				<li><a href='/portfolio/publications/mastersThesis/09_Appendix_03.pdf'>Appendix 3: Short Score of <u>Ecuatorial</u> (1.1 mb)</a></li>
 			</ul>
 		</dd>
 		<dt>One large PDF</dt>
