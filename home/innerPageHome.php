47ba20b75a44d8e2731baafd31366826bab735aa
Greg Sandell <GSandell@contractor.crocs.com>
2020-03-25 12:42:48 -0700

    Add margin around bio headshot

--- home/innerPageHome.php
+++ home/innerPageHome.php
@@ -6,7 +6,7 @@
 <h1>Frontend Software Engineer - SDET</h1>
 <p style='text-align: justify; float: left; padding-right: 0px'>
 <!-- TO DO - pull the copy directly from the Resume object -->
-	&nbsp;<img alt="Photo of Greg Sandell" style='float: right; padding-left: 5px' src='/image/gregHeadshot-50.jpg' />
+	&nbsp;<img alt="Photo of Greg Sandell" style='float: right; margin-left: 15px; margin-top: 7px;' src='/image/gregHeadshot-50.jpg' />
 	I provide client Web Application solutions of high quality and reliability using Javascript/Node.js/React Front End Development, Selenium-based Automated UI Testing, Salesforce Commerce Cloud eCommerce, load testing, SQL database design, asynchronous/multi-threaded processing, secure authentication, messaging, and integrating third party software.  I have worked for enterprise clients in retail eCommerce, Real Estate, Executive Search, Healthcare, AdTech, Publishing, Finance, Defense, and Travel. I am also an accomplished classical pianist, electric bass player, and composer.
 </p>
 <h2>Switch to the <a href="/j">Mobile version</a> of the site, written in jQuery Mobile!</h2>
